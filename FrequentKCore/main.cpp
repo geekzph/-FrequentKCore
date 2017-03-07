c1fcc4b75b04836dcabc43114ef98ec1911b16f6
geekzph <geekzph@qq.com>
2017-03-07 19:19:59 +0800

    Initial Commit

---
+++ FrequentKCore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FrequentKCore
+//
+//  Created by zph on 2017/3/7.
+//  Copyright © 2017年 zph. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
